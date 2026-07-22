e5af4cc29f5d410edfeb8cdea7ce540a2e7f212f
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-07-22 22:47:56 +0200

    Create elfakSubject.py

---
+++ elfakSubject.py
@@ -0,0 +1,93 @@
+subjectList = {
+
+  "aip" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=100&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+
+  "uur" : {
+    "url" :"https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=84&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+
+  "lp" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=115&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+    
+  "dmat" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=17&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+  
+  "oop" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=41&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+
+  "pj" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=82&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+  
+  "sp" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=114&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+  
+  "tg" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=54&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+
+  "is" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=24&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+  
+  "os" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=108&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  }, 
+  
+  "ps" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=74&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+  
+  "rm" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=86&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+  
+  "rmas" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=77&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+  
+  "si" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=2&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+  
+  "ti" : {
+    "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=78&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
+    "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
+  },
+
+  "oet1" : {
+    "url" : "https://eth.elfak.ni.ac.rs/?cat=4",
+    "xpath" : '//*[@id="content"]'
+  },
+
+  "oet2" : {
+    "url" : "https://eth.elfak.ni.ac.rs/?cat=5",
+    "xpath" : '//*[@id="content"]'
+  },
+
+  "fizika" : {
+    "url" : "https://mikro.elfak.ni.ac.rs/predmeti/fizika/",
+    "xpath" : '//*[@id="main"]'
+  }
+
+}
