56f6961bb9baa256ce2b70144af2c658a9f411d1
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-07-23 18:27:28 +0200

    Update elfakSubject.py

--- elfakSubject.py
+++ elfakSubject.py
@@ -95,7 +95,7 @@ pagesList = {
     "xpath" : '//*[@id="main"]'
   },
 
-  "csMainForum" : {
+  "katedra" : {
     "url" : "https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=1&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
     "xpath" : "/html/body/div[1]/div[2]/div/div[1]/div/div/article[1]/div/div/header/div[2]/div/time"
   }
